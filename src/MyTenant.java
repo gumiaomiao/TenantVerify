eda77d802a13462b909593ac7f0636744da3e10f
gumiaomiao <leslieguchenxia@163.com>
2025-04-19 23:01:23 +0800

    Update MyTenant.java
    隐去名字

--- src/MyTenant.java
+++ src/MyTenant.java
@@ -3,27 +3,27 @@ public class MyTenant {
     private TenantManager manager = new TenantManager("1", "Root Tenant");
     private Tenant rootTenant = manager.getRootTenant();
 
-    //生成租户并添加云资源，并按照租户体系，为规划总院-数智研究院-基础中心添加云资源
+    //生成租户并添加云资源，并按照租户体系，为规划总-数智研究-基础中添加云资源
     public void addTenants() {
-        // 添加二级租户：规划总院
-        Tenant planningInstitute = new Tenant("2", "规划总院", 2, rootTenant);
+        // 添加二级租户：规划总
+        Tenant planningInstitute = new Tenant("2", "规划总", 2, rootTenant);
         rootTenant.addChildTenant(planningInstitute);
 
-        // 添加三级租户：数智研究院
-        Tenant digitalResearchInstitute = new Tenant("3", "数智研究院", 3, planningInstitute);
+        // 添加三级租户：数智研究
+        Tenant digitalResearchInstitute = new Tenant("3", "数智研", 3, planningInstitute);
         planningInstitute.addChildTenant(digitalResearchInstitute);
 
-        // 添加四级租户：基础中心
-        Tenant basicCenter = new Tenant("4", "基础中心", 4, digitalResearchInstitute);
+        // 添加四级租户：基础中
+        Tenant basicCenter = new Tenant("4", "基础中", 4, digitalResearchInstitute);
         digitalResearchInstitute.addChildTenant(basicCenter);
 
-        // 添加四级租户：算力项目组
-        Tenant computingPowerProjectGroup = new Tenant("5", "算力项目组", 4, digitalResearchInstitute);
+        // 添加四级租户：算力项目
+        Tenant computingPowerProjectGroup = new Tenant("5", "算力项目", 4, digitalResearchInstitute);
         digitalResearchInstitute.addChildTenant(computingPowerProjectGroup);
 
         // 为基础中心和算力项目组分别添加 10T 的存储云资源
-        Tenant targetBasicCenter = manager.findTenantByName("基础中心");
-        Tenant targetComputingPowerProjectGroup = manager.findTenantByName("算力项目组");
+        Tenant targetBasicCenter = manager.findTenantByName("基础中");
+        Tenant targetComputingPowerProjectGroup = manager.findTenantByName("算力项目");
         if (targetBasicCenter != null) {
             targetBasicCenter.addResource("10T 存储云资源");
         }
